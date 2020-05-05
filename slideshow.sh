eac7538a387c52e9ca567bd42a647a3cf16bb630
Enrico Cirignaco <e.cirignaco@gmail.com>
2020-05-05 21:38:12 +0200

    Files's path for slideshow changed in order to work with the Background command "&".

--- slideshow.sh
+++ slideshow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #start slideshow
-feh --recursive --randomize --fullscreen  --quiet --hide-pointer --slideshow-delay 10 /media/pi/300GB/USA\ 2018\ Photos
+feh --recursive --randomize --fullscreen  --quiet --hide-pointer --slideshow-delay 10 "/media/pi/galery" &
