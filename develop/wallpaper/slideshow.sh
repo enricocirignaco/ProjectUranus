83d69d053e2dee8b1e9001d794b4a94f3abc9420
Enrico Cirignaco <e.cirignaco@gmail.com>
2020-05-05 22:20:45 +0200

    bugfix: wrong galery path.

--- develop/wallpaper/slideshow.sh
+++ develop/wallpaper/slideshow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #start slideshow
-feh --recursive --randomize --fullscreen  --quiet --hide-pointer --slideshow-delay 10 "/media/pi/galery" &
+feh --recursive --randomize --fullscreen  --quiet --hide-pointer --slideshow-delay 10 "/media/pi/300GB/galery" &
--- start_slideshow.sh
+++ start_slideshow.sh
@@ -3,7 +3,7 @@
 #mount HDD
 sudo mount -t exfat /dev/sda2 /media/pi
 #start slideshow
-feh --recursive --randomize --fullscreen  --quiet --hide-pointer --slideshow-delay 10 "/media/pi/galery" &
+feh --recursive --randomize --fullscreen  --quiet --hide-pointer --slideshow-delay 10 "/media/pi/300GB/galery" &
 #wait for slideshow to start
 sleep 1
 #turn on hdmi
