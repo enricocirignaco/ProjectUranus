4cd24ec9b35de80c8039ce31c88276a6d8c06553
Enrico Cirignaco <e.cirignaco@gmail.com>
2020-05-23 15:49:23 +0200

    option "zoom fill" added to command "feh" in order to fill wallpaper screen no matter what format the pictures have.

--- start_slideshow.sh
+++ start_slideshow.sh
@@ -5,7 +5,7 @@ sudo mount -t exfat /dev/sda2 /media/pi
 #start slideshow
 # variable DISPLAY must be defined in order for the script to work with crontab
 export DISPLAY=:0.0
-feh --recursive --randomize --fullscreen  --quiet --hide-pointer --slideshow-delay 10 "/media/pi/galery" &
+feh --recursive --randomize --fullscreen  --zoom fill --quiet --hide-pointer --slideshow-delay 10 "/media/pi/galery" &
 #wait for slideshow to start
 sleep 1
 #turn on hdmi
