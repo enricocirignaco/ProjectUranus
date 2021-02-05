2db9714d1d3efe6b82286c79be430ed4742ad6ff
Enrico Cirignaco <e.cirignaco@gmail.com>
2021-02-05 22:32:04 +0100

    setup script modified

--- slideshow/setup_wallpaper.sh
+++ slideshow/setup_wallpaper.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
-#install feh and exfat utilities
-apt install feh -y
-apt install exfat-fuse -y
-apt install exfat-utils -y
+# update system
+sudo apt-get update
+sudo apt-get upgrade
 
-#add cron jobs
+# install feh and exfat utilities
+sudo apt install feh -y
+sudo apt install exfat-fuse -y
+sudo apt install exfat-utils -y
+
+# Add following cronjobs to crontab:
+# start slideshow
+# end slideshow
+# update system once a month
+CRONJOB_START = " * * * /home/pi/Documents/mambocat/start_slideshow.sh > /dev/null 2>&1"
+CRONJOB_END = " * * * /home/pi/Documents/mambocat/end_slideshow.sh > /dev/null 2>&1"
+CRONJOB_UPDATE = "0 0 1 * * root (apt -y update && apt -y  upgrade) > /dev/null 2>&1"
+read -p "Enter slideshow start time (hh mm): " START_TIME
+read -p "Enter slideshow end time (hh mm): " END_TIME
+#write out current crontab
+sudo crontab -u pi -l > mycron
+#echo new cron into cron file
+echo "$START_TIME$CRONJOB_START" >> mycron
+echo "$END_TIME$CRONJOB_END" >> mycron
+echo "$CRONJOB_UPDATE" >> mycron
+#install new cron file
+sudo crontab -u pi mycron
+rm mycron
+
+# turn off screensaver
+sudo sed -i 's/# xserver-command=X/xserver-command=X -s 0 dpms/' /etc/lightdm/lightdm.conf
+
